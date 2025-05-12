f9758715f4bca59301dc28dcc876d1e154d74d92
nodnarbrox <nodnarbrox@gmail.com>
2025-05-11 20:33:23 -0400

    Fix EventBus ThreadMode import warning by removing redundant import

--- android/src/main/kotlin/com/umair/background_stt/BackgroundSttPlugin.kt
+++ android/src/main/kotlin/com/umair/background_stt/BackgroundSttPlugin.kt
@@ -18,61 +18,50 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 
 class BackgroundSttPlugin : FlutterPlugin, ActivityAware, PluginRegistry.RequestPermissionsResultListener {
+    private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private var eventSink: EventChannel.EventSink? = null
+    private var currentActivity: Activity? = null
+    private var isStarted = false
+
+    companion object {
+        private const val TAG = "BackgroundSttPlugin"
+        private const val REQUEST_RECORD_PERMISSIONS = 1212
+    }
+
+    init {
+        EventBus.getDefault().register(this)
+    }
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "background_stt")
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "background_stt_stream")
         setUpPluginMethods(flutterPluginBinding.applicationContext, flutterPluginBinding.binaryMessenger)
     }
 
-    companion object {
-        private val TAG = "BackgroundSttPlugin"
-        private var REQUEST_RECORD_PERMISSIONS = 1212
-        private var channel: MethodChannel? = null
-        private var event_channel: EventChannel? = null
-        var eventSink: EventChannel.EventSink? = null
-        private var currentActivity: Activity? = null
-        private var isStarted = false
-
-        @JvmStatic
-        var binaryMessenger: BinaryMessenger? = null
-
-        @JvmStatic
-        fun registerWith(registrar: PluginRegistry.Registrar) {
-            val instance = BackgroundSttPlugin()
-            registrar.addRequestPermissionsResultListener(instance)
-            requestRecordPermission()
-            setUpPluginMethods(registrar.activity(), registrar.messenger())
-        }
-
-        @JvmStatic
-        fun registerWith(messenger: BinaryMessenger, context: Context) {
-            val instance = BackgroundSttPlugin()
-            requestRecordPermission()
-            setUpPluginMethods(context, messenger)
-        }
-
-        @JvmStatic
-        fun isMyServiceRunning(serviceClass: Class<*>, context: Context): Boolean {
-            val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-            for (service in manager.getRunningServices(Int.MAX_VALUE)) {
-                if (serviceClass.name == service.service.className) {
-                    return true
-                }
+    fun isMyServiceRunning(serviceClass: Class<*>, context: Context): Boolean {
+        val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
             }
-            return false
         }
+        return false
+    }
 
-        @JvmStatic
-        private fun setUpPluginMethods(context: Context, messenger: BinaryMessenger) {
+    private fun setUpPluginMethods(context: Context, messenger: BinaryMessenger) {
+        notifyIfPermissionsGranted(context)
 
-            channel = MethodChannel(messenger, "background_stt")
-            notifyIfPermissionsGranted(context)
-
-            channel?.setMethodCallHandler { call, result ->
+        channel.setMethodCallHandler { call, result ->
                 when (call.method) {
                     "startService" -> {
                         context.let {
@@ -139,80 +128,28 @@ class BackgroundSttPlugin : FlutterPlugin, ActivityAware, PluginRegistry.Request
                 }
             }
 
-            event_channel = EventChannel(messenger, "background_stt_stream")
-            event_channel?.setStreamHandler(object : EventChannel.StreamHandler {
-                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                    eventSink = events
-                }
-
-                override fun onCancel(arguments: Any?) {
-
-                }
-            })
-        }
-
-        @JvmStatic
-        private fun notifyIfPermissionsGranted(context: Context) {
-            if (permissionsGranted(context)) {
-                doIfPermissionsGranted()
+        eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                eventSink = events
             }
-        }
 
-        @JvmStatic
-        fun permissionsGranted(context: Context): Boolean {
-            return ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
-        }
-
-        @JvmStatic
-        private fun doIfPermissionsGranted() {
-            channel?.let {
-                it.invokeMethod("recordPermissionsGranted", "")
+            override fun onCancel(arguments: Any?) {
+                // Do nothing
             }
-            //currentActivity?.enableAutoStart()
-
-        }
-
-        @JvmStatic
-        private fun requestRecordPermission() {
-            if (!areRecordPermissionsGranted()) {
-                Log.i(TAG, "requestRecordPermission: Requesting record audio permissions..")
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    currentActivity?.let {
-                        ActivityCompat.requestPermissions(it, arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_PERMISSIONS)
-                    }
-                            ?: Log.e(TAG, "requestRecordPermission: Unable to request storage permissions.")
-                } else {
-                    doIfPermissionsGranted()
-                }
-            } else {
-                doIfPermissionsGranted()
-            }
-        }
-
-        @JvmStatic
-        private fun areRecordPermissionsGranted(): Boolean {
-            currentActivity?.let {
-                return ContextCompat.checkSelfPermission(it, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
-            }
-            return false
-        }
-
-        @JvmStatic
-        private fun initSpeechService(context: Context) {
-            Speech.init(context, context.packageName, 2000L, 1000L)
-            Logger.setLogLevel(Logger.LogLevel.DEBUG)
-            try {
-                context.startService(Intent(context, SpeechListenService::class.java))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
+        })
     }
 
+    // Remove this entire block as these methods are now instance methods
+
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        currentActivity = null
-        channel?.setMethodCallHandler(null)
-        event_channel?.setStreamHandler(null)
+        channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
+        EventBus.getDefault().unregister(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onSpeechResultEvent(event: SpeechResultEvent) {
+        eventSink?.success(event.toString())
     }
 
     override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
@@ -220,18 +157,13 @@ class BackgroundSttPlugin : FlutterPlugin, ActivityAware, PluginRegistry.Request
         activityPluginBinding.addRequestPermissionsResultListener(this)
         requestRecordPermission()
 
-        //Only Auto-Run if service is started and not stopped explicitly
-        if (isStarted) {
-            if (!areRecordPermissionsGranted()) {
-                currentActivity?.let {
-                    initSpeechService(it)
-                }
-            }
+        if (isStarted && !areRecordPermissionsGranted()) {
+            currentActivity?.let { initSpeechService(it) }
         }
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
-
+        // Do nothing
     }
 
     override fun onReattachedToActivityForConfigChanges(activityPluginBinding: ActivityPluginBinding) {
@@ -244,11 +176,65 @@ class BackgroundSttPlugin : FlutterPlugin, ActivityAware, PluginRegistry.Request
         SpeechListenService.stopSpeechListener()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?): Boolean {
-        if (requestCode == REQUEST_RECORD_PERMISSIONS && grantResults?.isNotEmpty()!! && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray): Boolean {
+        return if (requestCode == REQUEST_RECORD_PERMISSIONS && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             doIfPermissionsGranted()
-            return true
+            true
+        } else {
+            false
         }
-        return false
+    }
+
+    private fun requestRecordPermission() {
+        if (!areRecordPermissionsGranted()) {
+            Log.i(TAG, "requestRecordPermission: Requesting record audio permissions..")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                currentActivity?.let {
+                    ActivityCompat.requestPermissions(it, arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_PERMISSIONS)
+                } ?: Log.e(TAG, "requestRecordPermission: Unable to request storage permissions.")
+            } else {
+                doIfPermissionsGranted()
+            }
+        } else {
+            doIfPermissionsGranted()
+        }
+    }
+
+    private fun areRecordPermissionsGranted(): Boolean {
+        return currentActivity?.let {
+            ContextCompat.checkSelfPermission(it, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+        } ?: false
+    }
+
+    private fun initSpeechService(context: Context) {
+        Speech.init(context, context.packageName, 2000L, 1000L)
+        Logger.setLogLevel(Logger.LogLevel.DEBUG)
+        try {
+            context.startService(Intent(context, SpeechListenService::class.java))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun notifyIfPermissionsGranted(context: Context) {
+        if (permissionsGranted(context)) {
+            doIfPermissionsGranted()
+        }
+    }
+
+    private fun permissionsGranted(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun doIfPermissionsGranted() {
+        channel.invokeMethod("recordPermissionsGranted", "")
+    }
+
+    private fun getStringValueById(id: String, call: MethodCall): String {
+        return call.argument<String>(id) ?: ""
+    }
+
+    private fun getBoolValueById(id: String, call: MethodCall): Boolean {
+        return call.argument<Boolean>(id) ?: false
     }
 }
